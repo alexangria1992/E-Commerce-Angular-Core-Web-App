cc3bb8ab2d8c805b93b0b0c4301e0842b8bc882a
alexangria1992 <alex040892@gmail.com>
2022-04-17 20:46:32 +1200

    Adding EF to csproj file and Created Database tables/classes

---
+++ E-Commerce-Core-Angular/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Core_Angular.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+
+
+    }
+}
---
+++ E-Commerce-Core-Angular/Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Core_Angular.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public virtual Category Categories { get; set; }
+
+    }
+}
